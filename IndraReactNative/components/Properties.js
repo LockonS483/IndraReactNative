b73d57f4f40a658ed94991b964f07c723305ca1a
LockonS483 <rumblie.gg@gmail.com>
2020-12-12 10:10:24 -0500

    wip properties fields

--- IndraReactNative/components/Properties.js
+++ IndraReactNative/components/Properties.js
@@ -1,20 +1,62 @@
-import React from 'react'
-import { Text, View, Dimensions } from 'react-native'
-
+import React, { Component } from 'react'
+import { View, Text, Form, StyleSheet, ScrollView, Dimensions } from 'react-native'
+import axios from 'axios'
+import config from '../config'
+import ModelInputfield from './ModelInputfield.js'
 
 var width = Dimensions.get('window').width;
 var height = Dimensions.get('window').height;
 
-function Properties({route, navigation}) {
+class Properties extends Component {
+    constructor(props) {
+        super(props);
+        const {route, navigation} = this.props;
+        this.state = { modelID : route.params.modelID, menuDetails : {}};
+        this.props_url = config.PROPS_URL;
+    }
+
+    async componentDidMount(){
+        const properties = await axios.get(`${this.props_url}${this.state.modelID}`)
+        this.setState({modelDetails : properties.data});
+        console.log(this.state.modelDetails);
+    }
+
+    render() {
+        return (
+            <View>
+                <Text>ModelID: {this.state.modelID}</Text>
+            {/*<Form>
+                <View className="container">
+                {Object.keys(this.state.modelDetails).map((item) => {
+                    return (
+                        <ModelInputfield
+                        label={this.state.modelDetails[item].question}
+                        type={this.state.modelDetails[item].atype}
+                        placeholder={this.state.modelDetails[item].val}
+                        error={this.state.modelDetails[item].errorMessage}
+                        propChange={this.propChanged}
+                        name={item}
+                        key={item}
+                        />
+                        <Text>hi</Text>
+                    );
+                })}
+                </View>
+            </Form>*/}
+            </View>
+        );
+    }
+}
+
+
+/*function Properties({route, navigation}) {
     const { modelID } = route.params
-    console.log(modelID)
+    
     return (
         <View>
             <Text>ModelID: {modelID}</Text>
         </View>
-    )
+    );
     
- }
+ }*/
  export default Properties
-
- 
--- IndraReactNative/config.js
+++ IndraReactNative/config.js
@@ -1,9 +1,11 @@
 let API_URL = 'https://indrasnet.pythonanywhere.com/';
+let PROPS_URL = API_URL + 'models/'
 
 if (process.env.REACT_APP_API_URL) {
   API_URL = process.env.REACT_APP_API_URL;
+  PROPS_URL = process.env.REACT_APP_API_URL + 'models/';
 }
 
-const config = { API_URL };
+const config = { API_URL, PROPS_URL };
 
 export default config;
