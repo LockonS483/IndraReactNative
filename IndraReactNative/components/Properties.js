002224db6a0e94adc488053bbb5603ce3c7336aa
LockonS483 <rumblie.gg@gmail.com>
2020-12-12 20:48:28 -0500

    Properties correctly loads api data

--- IndraReactNative/components/Properties.js
+++ IndraReactNative/components/Properties.js
@@ -7,25 +7,37 @@ import ModelInputfield from './ModelInputfield.js'
 var width = Dimensions.get('window').width;
 var height = Dimensions.get('window').height;
 
+
+
 class Properties extends Component {
     constructor(props) {
         super(props);
         const {route, navigation} = this.props;
-        this.state = { modelID : route.params.modelID, menuDetails : {}};
+        this.props_url = config.PROPS_URL;
+        this.state = { modelID : route.params.modelID, menuDetails : {}, loadingText : 'loading properties...', ready : false};
         this.props_url = config.PROPS_URL;
     }
 
     async componentDidMount(){
-        let properties = await axios.get(`${this.props_url}${this.state.modelID}`)
-        this.setState({modelDetails : properties.data});
-        console.log(this.state.modelDetails);
+        let properties = await axios.get(`${this.props_url}${this.state.modelID}`);
+        this.setState( { modelDetails : properties.data, ready : true });
+        this.setState( { ready: true } );
+        /*try{
+            axios.get(`${this.props_url}${this.state.modelID}`)
+            .then((res) => {
+                console.log(res);
+                this.setState({ready: true});
+            });
+        } catch(e) {
+            console.log(e);
+        }*/
     }
 
     render() {
-        return (
-            <View>
-                <Text>ModelID: {this.state.modelID}</Text>
-            <Form>
+        const ready = this.state.ready;
+        let t = <Text>{this.state.loadingText}</Text>;
+        if(ready){
+            /*t = <Form>
                 <View className="container">
                 {Object.keys(this.state.modelDetails).map((item) => {
                     return (
@@ -39,9 +51,24 @@ class Properties extends Component {
                         key={item}
                         />
                     );
+
+                    return(
+                        <Text></Text>
+                    );
                 })}
                 </View>
-            </Form>
+            </Form>*/
+
+            let texto = JSON.stringify(this.state.modelDetails);
+            t = <Text>
+                {texto}
+            </Text>
+        }
+
+        return (
+            <View>
+            <Text>ModelID: {this.state.modelID}</Text>
+            {t}
             </View>
         );
     }
--- IndraReactNative/config.js
+++ IndraReactNative/config.js
@@ -1,9 +1,9 @@
 let API_URL = 'https://indrasnet.pythonanywhere.com/';
-let PROPS_URL = API_URL + 'models/'
+let PROPS_URL = API_URL + 'models/props/'
 
 if (process.env.REACT_APP_API_URL) {
   API_URL = process.env.REACT_APP_API_URL;
-  PROPS_URL = process.env.REACT_APP_API_URL + 'models/props';
+  PROPS_URL = process.env.REACT_APP_API_URL + 'models/props/';
 }
 
 const config = { API_URL, PROPS_URL };
